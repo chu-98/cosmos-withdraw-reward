700eab550b4772860f9fc2eb22c90a937d248e0c
chu-98 <81007078+chu-98@users.noreply.github.com>
2022-05-29 00:10:46 +0900

    Keplr 지갑 연결 성공
    짜릿하다...
    거의 6시간 넘게 사용해서 핵심 기능인 '프론트에 지갑 연결'을 성공했다🙌🏼

---
+++ cosmos-withdraw-reward/src/components/main/Claim.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Claim() {
+  return (
+    <div>
+      <h1>Claim</h1>
+    </div>
+  );
+}
+
+export default Claim;
--- cosmos-withdraw-reward/src/components/main/Home.jsx
+++ cosmos-withdraw-reward/src/components/main/Home.jsx
@@ -1,10 +1,71 @@
+import { SigningCosmosClient } from "@cosmjs/launchpad";
 import React from "react";
+import styled from "styled-components";
+import Claim from "./Claim";
+
+const Header = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  margin: 25px 20px;
+  font-size: 30px;
+`;
+const Container = styled.div``;
+const Name = styled.div`
+  padding: 10px;
+  border: 2px solid #fff;
+  margin-right: 12px;
+`;
+const Connect = styled.div`
+  padding: 10px;
+  border: 2px solid #fff;
+`;
+const Btn = styled.button`
+  background: inherit;
+  border: none;
+  box-shadow: none;
+  border-radius: 0;
+  overflow: visible;
+  cursor: pointer;
+  font-size: 30px;
+  color: #fff;
+`;
+// const chainId = "osmosis";
+// console.log(window.keplr.enable(chainId));
 
 function Home() {
+  const connectWallet = async () => {
+    if (!window.keplr) {
+      alert("Please install keplr extension");
+    } else {
+      const chainId = "cosmoshub-4";
+      await window.keplr.enable(chainId);
+
+      const offlineSigner = window.keplr.getOfflineSigner(chainId);
+      const accounts = await offlineSigner.getAccounts();
+      const cosmJS = new SigningCosmosClient(
+        "https://lcd-cosmoshub.keplr.app",
+        accounts[0].address,
+        offlineSigner
+      );
+      console.log(cosmJS);
+    }
+  };
+
   return (
     <div>
-      <h1>HOME</h1>
-      <p>Welcome!</p>
+      <Header>
+        <Name>Wallet Name</Name>
+        <Name></Name>
+        <Connect>
+          <Btn onClick={connectWallet}>Keplr Connect</Btn>
+        </Connect>
+      </Header>
+      <Container>
+        <Claim />
+        <Claim />
+        <Claim />
+        <Claim />
+      </Container>
     </div>
   );
 }
